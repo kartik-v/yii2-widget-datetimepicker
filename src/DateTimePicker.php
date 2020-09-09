kartik-v/yii2-widget-datetimepicker

commit 22d8c40665d59b76383e658de6ef2c187ed0da14

Update to release v1.4.7

src/DateTimePicker.php
@@ -159,6 +159,12 @@ class DateTimePicker extends InputWidget
             $this->pluginOptions['timezone'] = Yii::$app->getTimeZone();
         }
         $isBs4 = $this->isBs4();
+        if ($this->type == self::TYPE_INLINE) {
+            $this->pluginOptions['linkField'] = $this->options['id'];
+            if (!empty($this->pluginOptions['format'])) {
+                $this->pluginOptions['linkFormat'] = $this->pluginOptions['format'];
+            }
+        }
         $this->pluginOptions = array_replace_recursive([
             'icontype' => $isBs4 ? 'fas' : 'glyphicon',
             'fontAwesome' => $isBs4,
@@ -211,12 +217,6 @@ class DateTimePicker extends InputWidget
             DateTimePickerAsset::registerBundle($view, $this->bsVersion)->js[] = $this->_langFile;
         } else {
             DateTimePickerAsset::registerBundle($view, $this->bsVersion);
-        }
-        if ($this->type == self::TYPE_INLINE) {
-            $this->pluginOptions['linkField'] = $this->options['id'];
-            if (!empty($this->pluginOptions['format'])) {
-                $this->pluginOptions['linkFormat'] = $this->pluginOptions['format'];
-            }
         }
         if ($this->type == self::TYPE_INPUT) {
             $this->registerPlugin($this->pluginName);

commit f06e59da94e98b5ae8cd1b6be65158ac68e7da8d

Fixing linkFormat is set before rendering of input
Fixes issue: https://github.com/kartik-v/yii2-datecontrol/issues/136
introduced by https://github.com/kartik-v/yii2-widget-datetimepicker/commit/22d8c40665d59b76383e658de6ef2c187ed0da14

src/DateTimePicker.php
@@ -160,13 +160,8 @@ class DateTimePicker extends InputWidget
         if ($this->autoDefaultTimezone && empty($this->pluginOptions['timezone']) && !empty(Yii::$app->getTimeZone())) {
             $this->pluginOptions['timezone'] = Yii::$app->getTimeZone();
         }
+        
         $isBs4 = $this->isBs4();
-        if ($this->type == self::TYPE_INLINE) {
-            $this->pluginOptions['linkField'] = $this->options['id'];
-            if (!empty($this->pluginOptions['format'])) {
-                $this->pluginOptions['linkFormat'] = $this->pluginOptions['format'];
-            }
-        }
         $this->pluginOptions = array_replace_recursive([
             'bootcssVer' => 3,
             'icontype' => $isBs4 ? 'fas' : 'glyphicon',
@@ -228,6 +223,12 @@ class DateTimePicker extends InputWidget
             DateTimePickerAsset::registerBundle($view, $this->bsVersion)->js[] = $this->_langFile;
         } else {
             DateTimePickerAsset::registerBundle($view, $this->bsVersion);
+        }
+        if ($this->type == self::TYPE_INLINE) {
+            $this->pluginOptions['linkField'] = $this->options['id'];
+            if (!empty($this->pluginOptions['format'])) {
+                $this->pluginOptions['linkFormat'] = $this->pluginOptions['format'];
+            }
         }
         $el = "jQuery('#" . $this->options['data-datetimepicker-source'] . "')";
         $this->registerPlugin($this->pluginName, $el);